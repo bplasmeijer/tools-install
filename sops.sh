8b1a42009325df5401d63d9ebeffd0cb9fa63432
bart-plasmeijer <bap@sitecore.net>
2024-05-01 09:43:41 +0200

    update sops ghe repo

--- sops.sh
+++ sops.sh
@@ -2,15 +2,14 @@
 set -e
 source <(curl -sSL benco.io/lib) # Load libary from remote URL, it's safe!
 
-GITHUB="mozilla/sops"
+GITHUB="getsops/sops"
 VERSION=${1:-"$(get_latest_release $GITHUB)"}
 INSTALL_DIR=${2:-"$HOME/.local/bin"}
 CMD=sops
 NAME="Mozilla Sops"
-
+echo VERSION
 pre_run
-
-curl -sL "https://github.com/${GITHUB}/releases/download/v${VERSION}/sops-v${VERSION}.linux" -o "$INSTALL_DIR"/sops
+curl -sL "https://github.com/${GITHUB}/releases/download/v${VERSION}/sops-v${VERSION}.linux.amd64" -o "$INSTALL_DIR"/sops
 chmod +x "$INSTALL_DIR"/sops
 
 post_run
